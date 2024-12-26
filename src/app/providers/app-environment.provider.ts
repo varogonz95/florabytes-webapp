162be8f1f47dbf197c9e694f51ca04e50dda041c
varogonz95 <varogonz95@gmail.com>
2024-12-26 14:11:13 +0000

    move environment provider to /providers folder

---
+++ src/app/providers/app-environment.provider.ts
@@ -0,0 +1,14 @@
+import { InjectionToken, EnvironmentProviders, makeEnvironmentProviders } from "@angular/core";
+import { IAppEnvironment } from "./app-environment";
+import { environment } from "../../environments/environment";
+
+export const APP_ENVIRONMENT = new InjectionToken<IAppEnvironment>('APP_ENVIRONMENT');
+
+export function provideAppEnvironment(): EnvironmentProviders {
+    return makeEnvironmentProviders([
+        {
+            provide: APP_ENVIRONMENT,
+            useValue: environment
+        }
+    ])
+}
---
+++ src/app/providers/app-environment.ts
@@ -0,0 +1,36 @@
+export interface IAppEnvironment {
+    maxPairingRetries?: number,
+    bypassBluetoothAdapterCheck?: boolean,
+    simulateMobileUserAgent?: boolean,
+    firebase: {
+        apiKey: string,
+        authDomain: string,
+        projectId: string,
+        storageBucket: string,
+        messagingSenderId: string,
+        appId: string,
+    }
+    imageProvider?: {
+        baseUrl: string,
+        getCollectionsQueryParams?: Record<string, any>
+    },
+    pgotchiFuncClient: {
+        baseAddress: string,
+        apiKey?: string
+    }
+    pgotchiHttpClient: {
+        baseAddress: string,
+    },
+    iotHub: {
+        hostName: string,
+        port: number,
+        eventHub: {
+            consumerGroup: string,
+            eventHubName: string
+            connectionString: string,
+        }
+    },
+    pgotchiSignalR?: {
+        negotiateEndpoint: string,
+    },
+}
